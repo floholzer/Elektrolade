4d3462f36fd704fd7e08af5ac7fdc722bb1932a1
Florentin Holzer <florentin.holzer@lb-systems.at>
2024-06-12 08:44:55 +0200

    Updated file structure & added modules for UI and API

---
+++ StationAPI/src/test/java/at/technikum/stationapi/StationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.stationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ StationUI/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module at.technikum.stationui {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens at.technikum.stationui to javafx.fxml;
+    exports at.technikum.stationui;
+}
