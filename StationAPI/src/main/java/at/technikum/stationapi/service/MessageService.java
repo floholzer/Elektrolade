6164b688856666c2630be71dbbf315379133fff9
Florentin Holzer <wi22b090@fh-technikum.at>
2024-06-22 11:05:34 +0200

    Updated and reworked SpringBootApp

--- StationAPI/src/main/java/at/technikum/stationapi/service/MessageService.java
+++ StationAPI/src/main/java/at/technikum/stationapi/service/MessageService.java
@@ -1,4 +1,36 @@
 package at.technikum.stationapi.service;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.UUID;
+import java.util.concurrent.TimeoutException;
+
+@Service
 public class MessageService {
+    private final ConnectionFactory factory = new ConnectionFactory();
+
+
+    public boolean send(String queueName, String message) throws IOException, TimeoutException {
+        factory.setHost("localhost");
+        factory.setPort(30003);
+        UUID uuid = UUID.randomUUID();
+        try (
+                Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel();
+             ) {
+
+            channel.queueDeclare(queueName, false, false, false, null);
+
+            channel.basicPublish("", queueName, null, message.getBytes());
+            System.out.println("[#" + uuid + "] Sent to '" + queueName + "', Message: '" + message + "'");
+        } catch (IOException | TimeoutException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
 }
